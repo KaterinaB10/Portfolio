19bda8083adf66ce9bd207d6c047473f03306f70
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-10 09:16:33 +0200

    Routing for pages is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { Layout } from "./pages/Layout";
+import { Landing } from "./pages/Landing";
+import { ProjectReview } from "./pages/project-review/ProjectReview";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/project/:?" element={<ProjectReview />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
