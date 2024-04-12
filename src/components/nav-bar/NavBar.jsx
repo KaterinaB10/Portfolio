780c7ef52534b5dac225bc0dfd3d4b2480023d10
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-12 11:52:52 +0200

    updated

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,6 +1,8 @@
 import styles from "./navBar.module.css";
 import { NavButton } from "./NavButton";
 import { ThemeSwitcher } from "./theme-switcher/ThemeSwitcher";
+import { GitHubLogo } from "./link-logos/GitHubLogo";
+import { LinkInLogo } from "./link-logos/LinkInLogo";
 
 export function NavBar() {
   return (
@@ -12,7 +14,14 @@ export function NavBar() {
           <NavButton title="Projects" />
           <NavButton title="Contact" />
         </div>
-        <div className={styles.navExternalLinks}></div>
+        <div className={styles.navExternalLinks}>
+          <a href="">
+            <GitHubLogo />
+          </a>
+          <a href="">
+            <LinkInLogo />
+          </a>
+        </div>
         <div className={styles.themeSwitcher}>
           <ThemeSwitcher />
         </div>
