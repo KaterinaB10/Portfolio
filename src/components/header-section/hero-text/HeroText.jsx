44aa6d8dcea145de458f7c53e5715996bd41e974
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-15 13:49:50 +0200

    hero-text-done

--- src/components/header-section/hero-text/HeroText.jsx
+++ src/components/header-section/hero-text/HeroText.jsx
@@ -3,8 +3,8 @@ import styles from "./heroText.module.css";
 export function HeroText() {
   return (
     <div className={styles.textContainer}>
-      <h1>Hello, I’m Kateryna.</h1>
-      <h1>Front End Developer</h1>
+      <h1 className={styles.textFirstLine}>Hello, I’m Kateryna.</h1>
+      <h1 className={styles.textSecondLine}>Front End Developer</h1>
       <p>
         I’m passionate about solving unique coding challenges and deeply drawn
         to creativity and problem-solving.
